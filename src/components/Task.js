004e4d07045ada94923b7c48010f03e19fedb8d2
MuthuramanChockalingam <ramsmart710@gmail.com>
2021-07-19 06:57:29 +0000

    Fixed the key error in component.

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,11 @@
 import { React } from 'react';
 
 const Task = ({ name, totalCost, tasks }) =>
-	<div className="task">
-		<div className="task-name">{ name }</div>
-		<div className="total-cost">{ totalCost }</div>
+	<div key={ name } className="task">
+		<div className="title-bar">
+			<div className="task-name">{ name }</div>
+			<div className="total-cost">{ totalCost }</div>
+		</div>
 		<div>{ tasks.map(Task) }</div>
 	</div>;
 
