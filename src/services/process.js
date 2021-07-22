f58b6b1769f2c770ee6971fb676cd70397fc7a27
MuthuramanChockalingam <ramsmart710@gmail.com>
2021-07-22 05:09:18 +0000

    Refactored process.js

--- src/services/process.js
+++ src/services/process.js
@@ -1,10 +1,12 @@
 const process = (task) => {
 	const tasks = (task.tasks || []).map(process);
 
-	return { name: task.name,
+	return {
+		name: task.name,
 		totalCost: (task.cost || 0) + tasks.reduce((a, childTask) =>
 			a + childTask.totalCost, 0),
-		tasks: tasks };
+		tasks: tasks,
+	};
 };
 
 export default process;
